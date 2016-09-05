76c79809f1f43bc8fec09db88e0ee4dc4e28d979
jiang6777 <jiang677@yeah.net>
2016-09-05 17:19:58 +0800

    Initial Commit

---
+++ UITabDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITabDemo
+//
+//  Created by hejiangshan on 16/9/5.
+//  Copyright © 2016年 飞兽科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
